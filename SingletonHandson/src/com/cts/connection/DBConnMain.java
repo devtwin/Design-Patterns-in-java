f04ecbc92e449e2d0a8e30726c7c580fbe301ab6
Subbarayudu <40607314+devtwin@users.noreply.github.com>
2021-03-22 08:00:03 +0530

    singleton pattern

---
+++ SingletonHandson/src/com/cts/connection/DBConnMain.java
@@ -0,0 +1,11 @@
+package com.cts.connection;
+
+public class DBConnMain {
+
+	public static void main(String[] args) {
+		DBConn instance=DBConn.getInstance();
+		System.out.println(instance);
+		
+	}
+
+}
